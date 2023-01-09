f257d95d0613b405d7277df4f1c2c47df83298ad
kalagbor.christian <kalagbor.christian@yahoo.com>
2023-01-09 14:57:36 +0100

    b

--- prov.tf
+++ prov.tf
@@ -1,5 +1,5 @@
 terraform infrastructure provisioning files
 used for AWS infrastructure provisioning
 congiguration file for EC2, VPC amd EKS
-config file for users and groups
-terraform state file
+
+
